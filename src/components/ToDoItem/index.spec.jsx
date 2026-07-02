f0b8b6efd4a654c9f79a6f89cccefed64fe9b2c3
braiancalpar <braian.santos@calpar.com.br>
2026-07-02 15:42:33 -0300

    Teste de botão com getBy padrão e test

--- src/components/ToDoItem/index.spec.jsx
+++ src/components/ToDoItem/index.spec.jsx
@@ -21,7 +21,7 @@ describe("ToDoItem", () => {
     expect(getByText("02/07/2026")).toBeInTheDocument();
     expect(getByRole("checkbox")).not.toBeChecked();
   });
-  test("deveria chamar a função selectTodoForEdit quando o botão for clicado", async () => {
+  test("deveria chamar a função selectTodoForEdit quando o botão de editar for clicado", async () => {
     const funcaoSimulandoSelectTodoForEdit = jest.fn();
     const item = {
       description: "Editar Jest",
@@ -40,5 +40,43 @@ describe("ToDoItem", () => {
     await userEvent.click(button);
 
     expect(funcaoSimulandoSelectTodoForEdit).toHaveBeenCalledWith(item);
+  });
+  test("deveria chamar a função removeTodo quando o botão de deletar for clicado", async () => {
+    const funcaoSimulandoRemoveTodo = jest.fn();
+    const item = {
+      description: "Excluir Jest",
+      createdAt: "2026-07-02T10:00:00.000Z",
+      completed: false,
+    };
+    const { getByRole } = render(
+      <TodoContext.Provider value={{ removeTodo: funcaoSimulandoRemoveTodo }}>
+        <ToDoItem item={item} />
+      </TodoContext.Provider>,
+    );
+
+    const button = getByRole("button", {
+      name: /delete/i,
+    });
+    await userEvent.click(button);
+
+    expect(funcaoSimulandoRemoveTodo).toHaveBeenCalledWith(item);
+  });
+  test("[data-testid] deveria chamar a função removeTodo quando o botão de deletar for clicado", async () => {
+    const funcaoSimulandoRemoveTodo = jest.fn();
+    const item = {
+      description: "Excluir Jest",
+      createdAt: "2026-07-02T10:00:00.000Z",
+      completed: false,
+    };
+    const { getByTestId } = render(
+      <TodoContext.Provider value={{ removeTodo: funcaoSimulandoRemoveTodo }}>
+        <ToDoItem item={item} />
+      </TodoContext.Provider>,
+    );
+
+    const button = getByTestId("btn-delete");
+    await userEvent.click(button);
+
+    expect(funcaoSimulandoRemoveTodo).toHaveBeenCalledWith(item);
   });
 });
